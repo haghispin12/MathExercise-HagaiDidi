4177350511579678aeb4f8a4b363650bc3bd51f1
user <hag.hispin12@gmail.com>
2023-11-07 19:30:19 +0200

    submit and user name

--- app/src/main/java/com/example/mathexercise_hagaididi/LoginActivity.java
+++ app/src/main/java/com/example/mathexercise_hagaididi/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.mathexercise_hagaididi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText UserName;
+    private Button submit;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+        UserName = findViewById(R.id.username);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
