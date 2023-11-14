5712e5b5963df34f98d5b71a94a8635a8f982bbb
user <hag.hispin12@gmail.com>
2023-11-14 19:33:00 +0200

    rate buttun

--- app/src/main/java/com/example/mathexercise_hagaididi/MainActivity.java
+++ app/src/main/java/com/example/mathexercise_hagaididi/MainActivity.java
@@ -30,15 +30,82 @@ public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModelMain;
     private TextView user;
     private TextView score;
-
+    private Button rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewModelMain = new ViewModelProvider(this).get(MainViewModel.class);
         initView();
+        viewModel();
         SetUserDetails();
+        activity();
+
+    }
+
+    private void initView()
+    {
+        math = findViewById(R.id.mathe);
+        chlng = findViewById(R.id.btnchlng);
+        x20 = findViewById(R.id.btn20x);
+        mulb = findViewById(R.id.btnmulb);
+        firstnum = findViewById(R.id.firstnum);
+        mul = findViewById(R.id.mul);
+        secondnum = findViewById(R.id.secondnum);
+        ans = findViewById(R.id.ans);
+        chk = findViewById(R.id.chk);
+        save = findViewById(R.id.save);
+        sau = findViewById(R.id.sau);
+        score =findViewById(R.id.score);
+        user = findViewById(R.id.User);
+        rate = findViewById(R.id.rate);
+
+    }
+    private void SetUserDetails(){
+    String userName = getIntent().getStringExtra("UserName");
+    user.setText(("welcome "+userName+"! current score: "));
+    viewModelMain.SetUserDetails(userName);
+
+    }
+
+    public void activity(){
+        chlng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModelMain.chalenge(3);
+                ans.setText("");
+            }
+        });
+        x20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModelMain.x20(2);
+                ans.setText("");
+            }
+        });
+        mulb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModelMain.mulltable(1);
+                ans.setText("");
+            }
+        });
+        chk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, viewModelMain.answer(ans), Toast.LENGTH_SHORT).show();
+            }
+        });
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void viewModel(){
+        viewModelMain = new ViewModelProvider(this).get(MainViewModel.class);
         viewModelMain.Vnum1.observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
@@ -58,58 +125,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-
-    private void initView()
-    {
-        math = findViewById(R.id.mathe);
-        chlng = findViewById(R.id.btnchlng);
-        chlng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               viewModelMain.chalenge(3);
-               ans.setText("");
-            }
-
-        });
-        x20 = findViewById(R.id.btn20x);
-        x20.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModelMain.x20(2);
-                ans.setText("");
-            }
-        });
-        mulb = findViewById(R.id.btnmulb);
-        mulb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               viewModelMain.mulltable(1);
-                ans.setText("");
-            }
-        });
-        firstnum = findViewById(R.id.firstnum);
-        mul = findViewById(R.id.mul);
-        secondnum = findViewById(R.id.secondnum);
-        ans = findViewById(R.id.ans);
-        chk = findViewById(R.id.chk);
-        chk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, viewModelMain.answer(ans), Toast.LENGTH_SHORT).show();
-            }
-        });
-        save = findViewById(R.id.save);
-        sau = findViewById(R.id.sau);
-        score =findViewById(R.id.score);
-        user = findViewById(R.id.User);
-
-    }
-    public void SetUserDetails(){
-    String userName = getIntent().getStringExtra("UserName");
-    user.setText(("welcome "+userName+"! current score: "));
-    viewModelMain.SetUserDetails(userName);
     }
 
 }
