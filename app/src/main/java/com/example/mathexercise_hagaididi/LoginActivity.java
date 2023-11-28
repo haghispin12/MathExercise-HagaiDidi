cfc1d0d57d416bab407b162946f9f4cfa4f51c54
user <hag.hispin12@gmail.com>
2023-11-28 14:48:37 +0200

    saving the userName

--- app/src/main/java/com/example/mathexercise_hagaididi/LoginActivity.java
+++ app/src/main/java/com/example/mathexercise_hagaididi/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,17 +19,23 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+        SharedPreferences sh = getSharedPreferences("MySharedPref",
+                MODE_PRIVATE);
         setContentView(R.layout.activity_login);
         UserName = findViewById(R.id.username);
         submit = findViewById(R.id.submit);
-
+        UserName.setText(sh.getString("name",""));
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            myEdit.putString("name", UserName.getText().toString());
+            myEdit.apply();
             Intent intent = new Intent(LoginActivity.this,MainActivity.class);
             intent.putExtra("UserName", UserName.getText().toString());
             startActivity(intent);
+
             }
         });
     }
